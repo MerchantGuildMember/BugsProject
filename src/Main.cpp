f0146a6aa666e65626f182eb43d37b76e93c4499
Merchant <128327771+merchantguildmember@users.noreply.github.com>
2026-04-29 22:00:51 +0100

    Created main files

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+//
+// Created by H on 29/04/2026.
+//
+
+#include "Main.h"
+
+#include <iostream>
+#include <ostream>
+
+int main() {
+
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
---
+++ src/Main.h
@@ -0,0 +1,13 @@
+//
+// Created by H on 29/04/2026.
+//
+
+#ifndef BUGSPROJECT_MAIN_H
+#define BUGSPROJECT_MAIN_H
+
+
+class Main {
+};
+
+
+#endif //BUGSPROJECT_MAIN_H
