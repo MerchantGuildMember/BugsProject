e7560519ea2a1557ca2902657d1fa2cfcf511941
Merchant <128327771+merchantguildmember@users.noreply.github.com>
2026-04-29 22:06:32 +0100

    Updated TODO list for menu items in main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,6 +7,18 @@
 #include <iostream>
 #include <ostream>
 
+/*
+ *  TODO:   Menu Items like
+ *  TODO:               1. Load Data from file
+ *  TODO:               2. Display all bugs
+ *  TODO:               3. Find a bug given an id
+ *  TODO:               4. Action "Tap" which causes all bugs to move and then fight/eat each other
+ *  TODO:               5. Display Life history of all bugs (path taken)
+ *  TODO:               6. Display all cells listing their bugs
+ *  TODO:               7. Run simulation ("Tap" action every second)
+ *  TODO:               8. Exit (Write Life history of all bugs to file)
+ */
+
 int main() {
 
     std::cout << "Hello World!" << std::endl;
