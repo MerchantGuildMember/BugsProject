75115ee6ed1a121536f28d511cfc68626f6a5aeb
Merchant <128327771+merchantguildmember@users.noreply.github.com>
2026-05-05 13:20:11 +0100

    copied from Crawler

--- src/Hopper.h
+++ src/Hopper.h
@@ -10,6 +10,77 @@
 
 
 class Hopper: public Bug {
+    public:
+        Hopper(int id, int x, int y, int dir, int health)
+        : Bug(id, x, y, dir, health) {}
+
+    void move() {
+        while (isWayBlocked()) {
+            // https://stackoverflow.com/questions/2999012/generating-random-enums
+            currentDirection = direction(rand() % 4);       // not sure if it'll work yet
+        }
+        switch (currentDirection) {
+            // Depending on direction, adds +1 or -1 to which item in the pair.
+            case NORTH: // If bug is facing NORTH, it will be going up towards 0 so decrement
+                std::cout  << "Crawler " << id << " moving from " << position.first << ", " << position.second
+                << " to " << position.first << ", " << position.second-1 << std::endl;
+                position.second -= 1;
+                break;
+
+            case EAST:  // If bug is facing EAST, it will be going away from 0 so increment
+                std::cout  << "Crawler " << id << " moving from " << position.first << ", " << position.second
+                << " to " << position.first+1 << ", " << position.second << std::endl;
+                position.first += 1;
+                break;
+
+            case SOUTH: // If bug is facing SOUTH, it will be going down away from 0 so increment
+                std::cout  << "Crawler " << id << " moving from " << position.first << ", " << position.second
+                << " to " << position.first << ", " << position.second+1 << std::endl;
+                position.second += 1;
+                break;
+
+            case WEST:  // If bug is facing WEST, it will be going left towards 0 so decrement
+                std::cout  << "Crawler " << id << " moving from " << position.first << ", " << position.second
+                << " to " << position.first-1 << ", " << position.second << std::endl;
+                position.first -= 1;
+                break;
+        }
+        path.push_back(position);
+
+    }
+
+    bool isWayBlocked() {
+        switch (currentDirection) {
+            case NORTH:
+                if (position.second == 0) {
+                    std::cout  << "Crawler " << id << " reached eastern edge, changing direction." << std::endl;
+                    return true;
+                }
+                break;
+            case EAST:
+                if (position.first == 9) {
+                    std::cout  << "Crawler " << id << " reached eastern edge, changing direction." << std::endl;
+                    return true;
+                }
+
+                break;
+            case SOUTH:
+                if (position.second == 9) {
+                    std::cout  << "Crawler " << id << " reached southern edge, changing direction." << std::endl;
+                    return true;
+                }
+
+                break;
+            case WEST:
+                if (position.first == 0) {
+                    std::cout  << "Crawler " << id << " reached western edge, changing direction." << std::endl;
+                    return true;
+                }
+        }
+
+        return false;
+    }
+
 };
 
 
