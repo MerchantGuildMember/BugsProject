da648b3d1e63a0ec50fc9570050d2e6d6f708c82
Merchant <128327771+merchantguildmember@users.noreply.github.com>
2026-05-07 22:25:52 +0100

    generate current calendar time as return value, convert epoch time to local time, formats broken time to string and into timestamp with the format we want then cocat prefix and add timestamp with extension .out

--- src/SaveFile.cpp
+++ src/SaveFile.cpp
@@ -15,6 +15,12 @@ extern std::vector<Bug*> bugs;
 extern std::map<int, std::vector<std::string>> killMap;
 
 void saveFile() {
+    // generate filename with timestamp
+    std::time_t now = std::time(nullptr);
+    std::tm* t = std::localtime(&now);
+    char timestamp[32];
+    std::strftime(timestamp, sizeof(timestamp), "%Y%m%d_%H%M%S", t);
+    std::string filename = "bugs_life_history_" + std::string(timestamp) + ".out";
 
 
 }
