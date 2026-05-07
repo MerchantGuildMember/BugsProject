258215f54868412588380f88cc0e5f8261732de1
Merchant <128327771+merchantguildmember@users.noreply.github.com>
2026-05-07 22:22:53 +0100

    imports, kill map and bug vector

--- src/SaveFile.cpp
+++ src/SaveFile.cpp
@@ -3,6 +3,16 @@
 //
 
 #include "SaveFile.h"
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <map>
+#include "Bug.h"
+
+extern std::vector<Bug*> bugs;
+extern std::map<int, std::vector<std::string>> killMap;
 
 void saveFile() {
 
